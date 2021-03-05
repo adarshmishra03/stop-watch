67f525117e9f61db4f69f1d68e8ea8d376715a65
Adarsh Mishra <adarshkaushik01@gmail.com>
2021-03-05 21:36:12 +0530

    Initial commit

---
+++ src/timer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+    componentDidMount(){
+        setInterval(this.ticker,1000)
+    }
+    ticker(){
+        this.setState({clock: new Date() - this.props.start})
+    }
+    constructor(props){
+        super(props);
+
+        this.state = {
+            clock:0
+        };
+        this.ticker= this.ticker.bind(this);
+    }
+    render(){
+        var clock= Math.round(this.state.clock/1000);
+        return(
+            <div>
+                <p>You have been on this since:</p>
+                <span>{clock}</span>
+                <p>Seconds.</p>
+            </div>
+        );    
+    }
+}
+
+
+export default Timer;
